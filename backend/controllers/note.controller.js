1b31152ff6546cb171dd7ed237e6809231ff46c9
Ashishprajapati2906 <prajapatiashu2906@gmail.com>
2025-09-16 16:09:17 +0530

    backend done

--- backend/controllers/note.controller.js
+++ backend/controllers/note.controller.js
@@ -24,3 +24,32 @@ export const getNotes = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const updateNote = async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    const updateNote = await Note.findByIdAndUpdate(
+      req.params.id,
+      { title, content },
+      { new: true }
+    );
+    if (!updateNote) {
+      return res.status(404).json({ message: "Note not updated" });
+    }
+    res.status(200).json(updateNote);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const deleteNote = async (req, res) => {
+  try {
+    const deleteNote = await Note.findByIdAndDelete(req.params.id);
+    if (!deleteNote) {
+      return res.status(404).json({ message: "Note not found" });
+    }
+    res.status(200).json({ message: "Note deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
